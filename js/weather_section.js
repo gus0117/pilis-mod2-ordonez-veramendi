05da44c6853e584a9ca5003c80b4a0b97325fea8
Brenda Veramendi <brendaveramendi@gmail.com>
2022-08-23 23:08:57 -0300

    await

--- js/weather_section.js
+++ js/weather_section.js
@@ -1,19 +1,20 @@
-const d = document;
+let d = document;
+function convert(degrees){
+return (degrees-273.15).toFixed(2) + "°";
+}
 
-function weather(name,description,temperature){
-
-    try {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q?=jujuy&lat=-24.19457&lon=-65.29712&appid=a73e9b9ea863da57a214ae11a9aa63ea')
-            .then(response => response.json())
-            .then(data => {
-                let nameValue = data['name'];
-                let temperatureValue = data['main']['temp'];
-                let descriptionValue = data['weather'][0]['description'];
-                console.log(nameValue, temperatureValue, descriptionValue)
-                
-    })
-  }  catch(err){
-    err => console.error(err)
-  }
-
+export async function weather(name,temperature,description){
+await fetch('https://api.openweathermap.org/data/2.5/weather?q?=jujuy&lat=-24.19457&lon=-65.29712&appid=a73e9b9ea863da57a214ae11a9aa63ea')
+        .then(response => response.json())
+        .then(data => {
+            let nameValue = data['name'];
+            let temperatureValue = data['main']['temp'];
+            let descriptionValue = data['weather'][0]['description'];
+            console.log(nameValue, temperatureValue, descriptionValue,data)
+            name.innerHTML= nameValue;
+            temperature.innerHTML= convert(temperatureValue);
+            description.innerHTML = descriptionValue;
+        })
+            
+        .catch(err => console.error(err))
 }
