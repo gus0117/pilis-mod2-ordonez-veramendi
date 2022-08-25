cc85f7e3611584ce51b493610893151f71fc73d4
Brenda Veramendi <brendaveramendi@gmail.com>
2022-08-25 18:51:10 -0300

    add celsius

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 let d = document;
-let $name = d.querySelector(".name-city");
-let $temperature =d.querySelector(".temperature");
-let $description =d.querySelector(".description-weather");
-function convert(degrees){
-return (degrees-273.15).toFixed(2) + "°";
-}
-d.addEventListener("DOMContentLoaded", e => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q?=jujuy&lat=-24.19457&lon=-65.29712&appid=a73e9b9ea863da57a214ae11a9aa63ea')
-        .then(response => response.json())
-        .then(data => {
-            let nameValue = data['name'];
-            let temperatureValue = data['main']['temp'];
-            let descriptionValue = data['weather'][0]['description'];
-            console.log(nameValue, temperatureValue, descriptionValue,data)
-            $name.innerHTML= nameValue;
-            $temperature.innerHTML= convert(temperatureValue);
-            $description.innerHTML = descriptionValue;
-           
-        
-        })
-            
-        .catch(err => console.error(err))
+const $name = d.querySelector(".name-city");
+const $description = d.querySelector(".description-weather");
+const $temperature = d.querySelector(".temperature");
 
+d.addEventListener("DOMContentLoaded",e=>{
+    weather();
 })
 
+function domWrite(nameValue,temperatureValue,descriptionValue) {
+    
+    $name.textContent=nameValue;
+    $temperature.textContent= temperatureValue + "°C";
+    $description.textContent = descriptionValue;
+}
 
 async function weather(){
     try{
