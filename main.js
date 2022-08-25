c4e4aa14f6f9e0f674f5e8243d105046e1bdee78
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2022-08-25 20:20:35 -0300

    Agregada funcionalidad JS al formulario registro

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ async function weather(){
         alert(err);
     }             
 }
+
+function onClick (event) {
+    event.preventDefault();
+
+    const register = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        buisnes: document.getElementById('buisnes').value,
+        phone: document.getElementById('phone').value
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(register),
+        headers: { "Content-type":"application/json; charset=UTF-8"}
+    })
+    .then( (response) => response.json())
+    .then( (json) => {
+        console.log(json);
+        Swal.fire(
+            'Enviado',
+            'Gracias por participar',
+            'Enviado con exito'
+        );
+        cleanForm();
+    })
+    .catch( (err) => console.log(err) );
+}
+
+function cleanForm(){
+    let form = document.getElementById("form");
+    form.reset();
+}
+
+let submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", onClick);
